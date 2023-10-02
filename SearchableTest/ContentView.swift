fb60b2c67c12ffd96edcbad0dd744daeda5cc6c4
An Byungwook <abw2619@icloud.com>
2023-10-02 15:51:38 +0900

    Initial Commit

---
+++ SearchableTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SearchableTest
+//
+//  Created by 안병욱의 mac on 2023/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchableTest/SearchableTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableTestApp.swift
+//  SearchableTest
+//
+//  Created by 안병욱의 mac on 2023/10/02.
+//
+
+import SwiftUI
+
+@main
+struct SearchableTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
